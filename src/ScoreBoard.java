de4223f2846f59854af8f48f2d07635989a5619e
Akshay <kumarsharmaa69@gmail.com>
2016-06-23 10:57:57 +0530

    Added methods to calculate totalWickets & updating methods to trigger update player attributes

--- src/ScoreBoard.java
+++ src/ScoreBoard.java
@@ -1,41 +1,84 @@
-import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class ScoreBoard {
     private static final int OVER_LENGTH = 6;
+    private Players batsmans;
+    private Players bowlers;
     private ArrayList<Over> overs;
 
-    public ScoreBoard(ArrayList<Over> overs) {
+    private ScoreBoard(ArrayList<Over> overs, Players batsmans, Players bowlers) {
         this.overs = overs;
+        this.batsmans = batsmans;
+        this.bowlers = bowlers;
     }
 
-    public int totalBallIn(double givenOvers){
-        int completedOvers = (int) ((givenOvers * 10)/ OVER_LENGTH);
-        int remainingBalls = (int) (givenOvers * 10 - completedOvers * 10);
+    private int totalRunsTill(double over) {
+        ArrayList<Ball> balls = getBallsIn(over);
+        int runs = 0;
+        for (Ball ball : balls) {
+            runs += ball.getRun();
+        }
+        return runs;
+    }
+
+    private int calculateTotalWicketsTill(double over) {
+        ArrayList<Ball> balls = getBallsIn(over);
+        int wickets = 0;
+        for (Ball ball : balls) {
+            wickets += ball.getWicket();
+        }
+        return wickets;
+    }
+
+    public static ScoreBoard create(ArrayList<Over> overs, Players batsmans, Players bowlers) {
+        ScoreBoard scoreBoard = new ScoreBoard(overs, batsmans, bowlers);
+        return scoreBoard;
+    }
+
+    private ArrayList<Ball> filterBallsUpto(int index, Over over) {
+        ArrayList<Ball> balls = new ArrayList<>(index);
+        for (int i = 0; i < index; i++) {
+            balls.add(over.getBallAt(i));
+        }
+        return balls;
+    }
+
+    private int calculateTotalBallsIn(double givenOvers) {
+        int completedOvers = (int) Math.floor(givenOvers);
+        int remainingBalls = (int) Math.round((givenOvers - completedOvers) * 10);
         return (completedOvers * OVER_LENGTH) + remainingBalls;
     }
 
-
-
-    private int totalRunsTill(double givenOvers) {
-        int givenBalls = totalBallIn(givenOvers);
-        int totalRuns = 0;
-        ArrayList<Over> oversList = overs;
+    private ArrayList<Ball> getBallsIn(double givenOvers) {
+        int totalNumberOfBalls = calculateTotalBallsIn(givenOvers);
+        ArrayList<Ball> balls = new ArrayList<>(totalNumberOfBalls);
         for (Over over : overs) {
-            if(!over.IsOfGreaterLength(givenBalls)) {
-                totalRuns += over.totalRuns(OVER_LENGTH);
+            if (over.consistBallsLessThan(totalNumberOfBalls)) {
+                balls.addAll(filterBallsUpto(OVER_LENGTH, over));
+                totalNumberOfBalls -= OVER_LENGTH;
+            } else {
+                balls.addAll(filterBallsUpto(totalNumberOfBalls, over));
+                return balls;
             }
-            else{
-                totalRuns += over.totalRuns(givenBalls);
-            }
-            givenBalls -= OVER_LENGTH;
         }
-        return totalRuns;
+        return balls;
+    }
+
+    private void update(ArrayList<Ball> balls) {
+        for (Ball ball : balls) {
+            updateRecordAccordingTo(ball);
+        }
+    }
+
+    private void updateRecordAccordingTo(Ball ball) {
+        batsmans.update(ball);
+        bowlers.update(ball);
     }
 
     public Displayer showUpTo(double over) {
+        update(getBallsIn(over));
         int totalRuns = totalRunsTill(over);
-        return new Displayer(totalRuns);
+        int totalWickets = calculateTotalWicketsTill(over);
+        return new Displayer(totalRuns, totalWickets, batsmans, bowlers);
     }
-
 }
