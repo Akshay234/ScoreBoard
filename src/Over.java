2d600961c8f17536d6d8e874bd1741a356c61cb2
Akshay <kumarsharmaa69@gmail.com>
2016-06-23 09:21:20 +0530

    Refactor the code and added method to get length and specific ball in Over.java

--- src/Over.java
+++ src/Over.java
@@ -1,27 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Over {
 
     private static final int OVER_LENGTH = 6;
-    private char[] balls;
+    private ArrayList<Ball> balls;
 
-    public Over(char[] balls) {
+    public Over(ArrayList<Ball> balls) {
         this.balls = balls;
     }
 
-    private double length() {
-        return balls.length;
+    public double length() {
+        return balls.size();
     }
 
     public int totalRuns(int givenBalls) {
         int runs = 0;
+
         for (int i = 0; i < givenBalls; i++) {
-            if (balls[i] != 'W' && balls[i] != 'w') {
-                runs += Integer.parseInt(String.valueOf(balls[i]));
+            if (!balls.get(i).equals("W") && !balls.get(i).equals("w")) {
+                runs += Integer.parseInt(String.valueOf(balls.get(i)));
             }
         }
         return runs;
     }
 
-    public boolean IsOfGreaterLength(double length) {
-        return this.length() > length;
+    public boolean consistBallsLessThan(double length) {
+        return this.length() < length;
+    }
+
+    public Ball getBallAt(int i) {
+        return balls.get(i);
     }
 }
