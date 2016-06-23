593991a8de3f8a43ea2972fba39a7f95e9df1533
Akshay <kumarsharmaa69@gmail.com>
2016-06-23 09:07:17 +0530

    Added dummy object of bowlers and batsmans for testing purpose in InputScanner | This change is just for temp purpose

--- src/InputScanner.java
+++ src/InputScanner.java
@@ -1,17 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputScanner {
 
     private static void display(String oversDetail, double overBuffer) throws Exception {
         InputParser inputParser = new InputParser(oversDetail);
-        if (inputParser.isValidCharacters()) {
-            ScoreBoard scoreBoard = new ScoreBoard(inputParser.getOvers());
+
+        if (inputParser.isValid()) {
+            ScoreBoard scoreBoard = ScoreBoard.create(inputParser.getOvers(),dummyBatsmans(), dummyBowlers());
             Displayer displayer = scoreBoard.showUpTo(overBuffer);
             System.out.println(displayer);
         } else {
             throw new Exception("Given [ " + oversDetail + " ] are Invalid");
         }
+    }
 
+    private static Players dummyBatsmans() {
+        ArrayList<String> batsmansList = new ArrayList<String>();
+        batsmansList.add("W");
+        batsmansList.add("X");
+        batsmansList.add("Y");
+        batsmansList.add("Z");
+        return Players.createBatsmans(batsmansList);
+
+    }
+
+    private static Players dummyBowlers() {
+        ArrayList<String> bowlersList = new ArrayList<>();
+        bowlersList.add("A");
+        bowlersList.add("B");
+        bowlersList.add("C");
+        bowlersList.add("D");
+        return Players.createBowlers(bowlersList);
     }
 
     public static void main(String[] args) throws Exception {
@@ -29,3 +49,4 @@ public class InputScanner {
         display(oversDetail, overBuffer);
     }
 }
+
