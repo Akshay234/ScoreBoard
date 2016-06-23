a8096c90f689c33e2e41f8b1e06306db4cc7919f
Akshay <kumarsharmaa69@gmail.com>
2016-06-23 10:54:52 +0530

    Added totalRuns, totalWickets, striker & nonStriker statistics in Displayer.java to show

--- src/Displayer.java
+++ src/Displayer.java
@@ -1,16 +1,38 @@
-import java.util.Arrays;
-
 public class Displayer {
     String[] details;// should be genric;
     private int totalRuns;
-    // can pass multiple fields to displayer and show them in formated way , things like wicket taker , on strike player, on onstrike player,
+    private int totalWickets;
+    private Players batsmans;
+    private Players bowlers;
+    // can pass multiple fields to displayer and show them in formated way , things like wicket taker , on strike player, on onstrike player.
 
-    public Displayer(int totalRuns) {
+    public Displayer(int totalRuns, int totalWickets, Players batsmans, Players bowlers) {
         this.totalRuns = totalRuns;
+        this.totalWickets = totalWickets;
+        this.batsmans = batsmans;
+        this.bowlers = bowlers;
     }
+
+    private String totalRunsTemplate() {
+        return ">>> Total runs : " + totalRuns;
+    }
+
+    private String totalWicketsTemplate() {
+        return ">>> Wickets : " + totalWickets;
+    }
+
+    private String playerOnStrikeStatistics() {
+        return "Player on the batting end is Mr. " + batsmans.onStrike().getName() + ", he has played a total of " +batsmans.onStrike().getBallsPlayed() + " scoring "+  batsmans.onStrike().scored() +" runs.";
+    }
+
+    private String playerOnNonStrikeStatistics() {
+        return "On the non-striker’s end is Mr. " + batsmans.onNonStrike().getName() + ", he has played a total of " +batsmans.onNonStrike().getBallsPlayed() + " scoring "+  batsmans.onNonStrike().scored() +" runs.";
+    }
+
 
     @Override
     public String toString() {
-        return "ScoreBoard {" + ", totalRuns=" + totalRuns + '}';
+        StringBuilder statistics = new StringBuilder();
+        return String.valueOf(statistics.append(totalRunsTemplate()).append("\n").append(totalWicketsTemplate()).append("\n").append(playerOnStrikeStatistics()).append("\n").append(playerOnNonStrikeStatistics()));
     }
 }
