1488a576a7db6b64f7b7f803f6fe386ac242ac8d
Akshay234 <kumarsharmaa69@gmail.com>
2016-06-25 01:30:05 +0530

    Removed ball number mapping from balls class

--- src/RunsTracker.java
+++ src/RunsTracker.java
@@ -1,25 +1,27 @@
-import java.util.HashMap;
+import ball.Ball;
+
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
-public class RunsTracker implements Observer,Calculable {
+public class RunsTracker implements Observer, Calculable {
 
-    private final HashMap<Integer, Integer> balls;
+    private final ArrayList<Integer> balls;
 
     public RunsTracker() {
-        balls = new HashMap<Integer, Integer>();
+        balls = new ArrayList<>();
     }
 
     @Override
     public void update(Observable o, Object ball) {
         Ball ballToTrack = (Ball) ball;
-        balls.put(ballToTrack.number(), ballToTrack.runs());
+        balls.add(ballToTrack.runs());
     }
 
     @Override
     public int totalUpto(int threshold) {
         int runs = 0;
-        for (int i = 1; i <= threshold; i++) {
+        for (int i = 0; i < threshold; i++) {
             runs += balls.get(i);
         }
         return runs;
--- src/WicketsTracker.java
+++ src/WicketsTracker.java
@@ -1,25 +1,27 @@
-import java.util.HashMap;
+import ball.Ball;
+
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
-public class WicketsTracker implements Observer,Calculable {
+public class WicketsTracker implements Observer, Calculable {
 
-    private final HashMap<Integer, Integer> balls;
+    private final ArrayList<Integer> balls;
 
     public WicketsTracker() {
-        balls = new HashMap<Integer, Integer>();
+        balls = new ArrayList<>();
     }
 
     @Override
     public void update(Observable o, Object ball) {
         Ball ballToTrack = (Ball) ball;
-        balls.put(ballToTrack.number(), ballToTrack.wicket());
+        balls.add(ballToTrack.wicket());
     }
 
     @Override
     public int totalUpto(int threshold) {
         int wickets = 0;
-        for (int i = 1; i <= threshold; i++) {
+        for (int i = 0; i < threshold; i++) {
             wickets += balls.get(i);
         }
         return wickets;
